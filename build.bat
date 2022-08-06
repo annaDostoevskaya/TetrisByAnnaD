3a5ac0bdf27ab06cc2a9a717f945db3711d6f5b0
Dostoevskaya Anna <iwantknow.aboutjt68h43@gmail.com>
2022-08-06 22:08:41 +0600

    modify build system

--- build.bat
+++ build.bat
@@ -8,7 +8,15 @@ set code=%cd%
 set lib_path=%code%\thirdparty\libs
 set include_path=%code%\thirdparty\includes
 
+if not exist build mkdir build
+
 pushd build
+
+if not exist SDL2.dll (
+  echo [BUILD] Please, put SDL2.dll in build dir. 
+  exit /B -1
+)
+
 cl %opts% %code%\game.cpp -Fegame %lib_path%\SDL2\SDL2.lib %lib_path%\SDL2\SDL2main.lib -I%include_path% shell32.lib /link /SUBSYSTEM:WINDOWS
 popd
 
