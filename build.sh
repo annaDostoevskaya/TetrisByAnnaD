4fc1e7d6fe2db8341079dfac22d09bb1056da8f5
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2022-09-04 12:46:11 -0400

    linux build system

--- build.sh
+++ build.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-# NOTE(annad): For pause run it:
-# read -p "Press [Enter] key to start backup..."
+## Author: github.com/annadostoevskaya
+## File: build.sh
+## August 30th 2022 12:43 pm 
+##
+## Description:
+## Before running this file, you need install the SDL
+## library on your system.
+## For this you can visit this page:
+## https://lazyfoo.net/tutorials/SDL/01_hello_SDL/linux/index.php
 
-CODE="/home/annad/projects/TetrisByAnnaD"
 
-SDL_FLAGS="-I/usr/include/SDL2 -L/usr/lib -pthread -lSDL2 -D_REENTRANT"
-GAME_FLAGS="-D_GAME_INTERNAL -D_GAME_LINUX"
+SRC=`pwd`
+SDL_FLAGS=`sdl2-config --cflags --libs`
+OUTPUT_DIR="build"
+O_FLAGS=-O3
+DEV_FLAGS=-D_GAME_INTERNAL
+OPTS="$DEV_FLAGS -D_GAME_LINUX"
 
-pushd build > /dev/null
+if [ ! -d "./$OUTPUT_DIR" ]
+then
+    mkdir ./$OUTPUT_DIR
+fi
 
-gcc $CODE/sdl_game.cpp -o sdl_game $SDL_FLAGS $GAME_FLAGS
-gcc $CODE/game.cpp -shared $SDL_FLAGS $GAME_FLAGS -o game.dll
+pushd $OUTPUT_DIR > /dev/null
+
+gcc $SRC/sdl_game.cpp -o sdl_game $SDL_FLAGS $GAME_FLAGS
+gcc $SRC/game.cpp -shared $SDL_FLAGS $GAME_FLAGS -o game.dll
 
 popd > /dev/null
 
